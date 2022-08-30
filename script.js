656ed43ce1ecc0868992ef1fd421bb1a9f496814
Francina Martín <francinakb@gmail.com>
2022-08-30 15:21:32 -0300

    desafio opcional 3

--- script.js
+++ script.js
@@ -1,48 +1,67 @@
-const arreglo = [function sumar (numero1, numero2){           
+
+const arreglo = [function sumar(numero1, numero2) {
     return numero1 + numero2
 },
 
-function restar (numero1, numero2){        
+function restar(numero1, numero2) {
     return numero1 - numero2
 },
 
-function multiplicar(numero1, numero2){         
+function multiplicar(numero1, numero2) {
     return numero1 * numero2
 },
 
-function dividir(numero1, numero2){          
+function dividir(numero1, numero2) {
     return numero1 / numero2
 }]
+let resultado = []
 
-let numero1, numero2, operacion
+function funcionOperation() {
+    let numerodeveces = prompt('cuantas veces quiere operar')
 
-do{
-    numero1 = parseFloat (prompt("Ingrese un numero"))
-    numero2 = parseFloat (prompt("Ingrese otro numero"))
-    operacion = prompt("Ingrese una operacion")
+    for (let i = 0; i < numerodeveces; i++) {
+        let numero1 = parseFloat(prompt("Ingrese un numero"))
+        let numero2 = parseFloat(prompt("Ingrese otro numero"))
+        let operacion = prompt("Ingrese una operacion")
 
-    if(isNaN(numero1) || isNaN(numero2)){
-        alert ("Ingresar numero validos")
+        if (isNaN(numero1) || isNaN(numero2)) {
+            alert("Ingresar numero validos")
+        }
+        else if (numero2 === 0 && operacion === "/") {
+            alert("No se puede dividir con 0")
+        } else {
+
+            switch (operacion) {
+                case "+":
+                    alert(arreglo[0](numero1, numero2))
+                resultado.push(arreglo[0](numero1, numero2))
+                    console.log(resultado)
+                    break
+                case "-":
+                    alert(arreglo[1](numero1, numero2))
+                    resultado.push(arreglo[1](numero1, numero2))
+
+                    break
+                case "*":
+                    alert(arreglo[2](numero1, numero2))
+                        resultado.push(arreglo[2](numero1, numero2))
+
+                    break
+                case "/":
+                    alert(arreglo[3](numero1, numero2))
+                            resultado.push(arreglo[3](numero1, numero2))
+
+                    break
+                default:
+                    alert("No se puede realizar esta operacion")
+                    resultado.push(arreglo[4](numero1, numero2))
+                    break
+            }
+        }
     }
-    if(numero2 === 0 && operacion === "/"){
-        alert ("No se puede dividir con 0")
-    }
-}while((isNaN(numero1) || isNaN(numero2)) || (numero2 === 0 && operacion === "/"))         
-
-switch(operacion){
-    case "+":
-    alert(arreglo[0](numero1, numero2))
-    break
-    case "-":
-    alert(arreglo[1](numero1, numero2))
-    break
-    case "*":
-    alert(arreglo[2](numero1, numero2))
-    break
-    case "/":
-    alert(arreglo[3](numero1, numero2))
-    break
-    default:
-    alert("No se puede realizar esta operacion")
-    break
+    alert(resultado)
 }
+
+funcionOperation()
+
+let operacion, number
